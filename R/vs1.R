56fb8d360b0d6399b3859bc1b7eacb06dc7e832b
MatildeUNIBO <matilde.martini7@unibo.it>
2024-04-03 15:37:16 +0200

    Random sp

--- R/vs1.R
+++ R/vs1.R
@@ -15,13 +15,35 @@ worldclim
 names(worldclim)
 plot(worldclim)
 
+Number_species <- 10
+for (i in 1:Number_species){
+  seed(i)
+  first_Virtual_species <-  generateRandomSp (worldclim,
+                                               convert.to.PA = FALSE,
+                                               # How to combine response functions
+                                               species.type = "multiplicative",
+                                                # Random approach between PCA and response function
+                                                 approach = "random",
+                                               # Response function
+                                               relations = "gaussian",
+                                               # Realistic species
+                                               realistic.sp = TRUE,
+                                               plot = FALSE)
 
-PA.points <- sampleOccurrences(my.first.species, n =
-                                 30
-                               , type =
-                                 "presence-absence"
-                               , sampling.area = c(
-                                 "South America"
-                                 ,
-                                 "Mexico"
-                               ))
+new.pres <-convertToPA(first_Virtual_species,
+                       beta = "random",
+                       alpha = -0.05, plot = FALSE,
+                       species.prevalence = 0.1)
+
+presence.points <- sampleOccurrences(new.pres,
+                                     n = 100,
+                                     type = "presence-absence",
+                                     sample.prevalence = 0.9,
+                                     error.probability = 0,
+                                     detection.probability = 1,
+                                     correct.by.suitability = TRUE,
+                                     plot = FALSE,
+                                     sampling.area="Italy")
+}
+
+
