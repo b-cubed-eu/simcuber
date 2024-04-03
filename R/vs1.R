fc23cbb70cafe14cf17fccf1599b041c2d5e072b
MatildeUNIBO <matilde.martini7@unibo.it>
2024-04-03 14:56:14 +0200

    Worldclim download

--- R/vs1.R
+++ R/vs1.R
@@ -1,1 +1,27 @@
 # vs
+# Loading required packages
+library(sf)
+library(ClimDatDownloadR)
+library(raster)
+library(virtualspecies)
+library(tidyverse)
+
+
+#Dowload bioclimatic variables from Worldclim
+
+worldclim <- getData("worldclim", var = "bio", res = 0.5, lon=5, lat=45)
+worldclim
+
+names(worldclim)
+plot(worldclim)
+
+
+PA.points <- sampleOccurrences(my.first.species, n =
+                                 30
+                               , type =
+                                 "presence-absence"
+                               , sampling.area = c(
+                                 "South America"
+                                 ,
+                                 "Mexico"
+                               ))
