8b45a8b135c7dcd6728b010dfaf7d0e469870b52
Sanne Govaert <44606923+sangovae@users.noreply.github.com>
2024-04-04 16:19:45 +0200

    remove the dependency on seed + uncomment working code

--- tests/testthat/test-sample_observations.R
+++ tests/testthat/test-sample_observations.R
@@ -1,11 +1,11 @@
-# Set seed for reproducibility
-withr:local_seed(123)
-
 # Simulate some occurrence data with coordinates and time points
-num_points <- 10
 occurrences <- data.frame(
-  lon = runif(num_points, min = -180, max = 180),
-  lat = runif(num_points, min = -90, max = 90),
+  lon = c(-67.7872072532773, -32.589017059654, -176.231839740649, -113.81417135708,
+          123.382554799318, -96.7817584611475, -93.9240159653127, -152.391180479899,
+          -91.5394759085029, 83.5686739906669),
+  lat = c(61.1581976618618, -33.7593303155154, 37.4922579992563, -42.2967949043959,
+          16.9817749271169, -3.36783591192216, -42.2941083367914, 11.6262782597914,
+          74.37388014514, 72.3373901098967),
   time_point = 0
 )
 points_sf1 <- sf::st_as_sf(occurrences, coords = c("lon", "lat"))
@@ -43,10 +43,10 @@ test_that("arguments are of the right class", {
                regexp = "`detection_probability` must be a numeric value between 0 and 1.",
                fixed = TRUE)
 
-  # # sampling_bias is a character vector
-  # expect_error(sample_observations(points_sf1, 0.5, TRUE),
-  #              regexp = "`points_sf2` must be a character vector of length 1",
-  #              fixed = TRUE)
+  # sampling_bias is a character vector
+  expect_error(sample_observations(points_sf1, 0.5, TRUE),
+               regexp = "`points_sf2` must be a character vector of length 1",
+               fixed = TRUE)
 
   # coordinate_uncertainty_meters is a numeric value
   expect_error(sample_observations(points_sf1,
