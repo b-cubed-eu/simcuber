c29103e0c573cacd4d486b145c2ae4166cb30258
PietrH <pieterhuy@gmail.com>
2024-04-04 14:16:12 +0200

    Style, fit within 80 cols

--- tests/testthat/test-simulate_timeseries.R
+++ tests/testthat/test-simulate_timeseries.R
@@ -27,9 +27,12 @@ test_that("arguments are of the right class", {
                regexp = "`seed` must be an numeric vector of length 1.",
                fixed = TRUE)
   # n_time_points is 1, temporal_autocorr must be NA
-  expect_error(simulate_timeseries(n_time_points = 1, temporal_autocorr = simulate_random_walk),
-               regexp = "When `n_time_points` is 1, `temporal_autocorr` must be NA.",
-               fixed = TRUE)
+  expect_error(
+    simulate_timeseries(n_time_points = 1,
+                        temporal_autocorr = simulate_random_walk),
+    regexp = "When `n_time_points` is 1, `temporal_autocorr` must be NA.",
+    fixed = TRUE
+  )
 })
 
 test_that("output length is correct", {
