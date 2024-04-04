01e6d2567baf7a3f8c91d20cb23f8d876a77b261
wlangera <ward.langeraert@inbo.be>
2024-04-04 07:53:06 +0200

    add examples

--- R/simulate_timeseries.R
+++ R/simulate_timeseries.R
@@ -23,13 +23,116 @@
 #' @export
 #'
 #' @examples
+#' library(simcuber)
+#' library(ggplot)
 #'
-#' timeseries <- simulate_timeseries(
+#' ## 1. Use the function simulate_random_walk()
+#' simulate_timeseries(
 #'   initial_average_abundance = 50,
 #'   n_time_points = 10,
 #'   temporal_autocorr = simulate_random_walk,
-#'   sd_step = 0.1)
+#'   sd_step = 1,
+#'   seed = 123)
 #'
+#' ## 2. Visualising multiple draws
+#' # Set seed for reproducibility
+#' set.seed(123)
+#'
+#' # Draw n_sim abundances from Poisson distribution using random walk
+#' n_sim <- 10
+#' n_time_points <- 50
+#' sd_step <- 1
+#' list_abundances <- vector("list", length = n_sim)
+#'
+#' # Loop n_sim times over simulate_timeseries()
+#' for (i in seq_len(n_sim)) {
+#'   abundances <- simulate_timeseries(
+#'     initial_average_abundance = 50,
+#'     n_time_points = n_time_points,
+#'     temporal_autocorr = simulate_random_walk,
+#'     sd_step = sd_step)
+#'
+#'   list_abundances[[i]] <- data.frame(
+#'     time = seq_along(abundances),
+#'     abundance = abundances,
+#'     sim = i
+#'     )
+#' }
+#'
+#' # Combine list of dataframes
+#' data_abundances <- do.call(rbind.data.frame, list_abundances)
+#'
+#' # Plot the simulated abundances over time using ggplot2
+#' ggplot(data_abundances, aes(x = time, y = abundance, colour = factor(sim))) +
+#'   geom_line() +
+#'   labs(x = "Time", y = "Species abundance",
+#'   title = paste(n_sim, "simulated abundances using random walk",
+#'                 "with sd =", sd_step)) +
+#'   scale_y_continuous(limits = c(0, NA)) +
+#'   scale_x_continuous(breaks = seq(0, n_time_points, 5)) +
+#'   theme_minimal() +
+#'   theme(legend.position = "")
+#'
+#' ## 3. Using your own function
+#' # You can also specify your own trend function, e.g. this linear function
+#' my_own_linear_function <- function(
+#'    initial_average_abundance = initial_average_abundance,
+#'    n_time_points = n_time_points,
+#'    coef) {
+#'   # Calculate new average abundances over time
+#'   time <- seq_len(n_time_points) - 1
+#'   lambdas <- initial_average_abundance + (coef * time)
+#'
+#'   # Identify where the lambda values become 0 or lower
+#'   zero_or_lower_index <- which(lambdas <= 0)
+#'
+#'   # If any lambda becomes 0 or lower, set all subsequent lambdas to 0
+#'   if (length(zero_or_lower_index) > 0) {
+#'     zero_or_lower_indices <- zero_or_lower_index[1]:n_time_points
+#'     lambdas[zero_or_lower_indices] <- 0
+#'   }
+#'
+#'   # Return average abundances
+#'   return(lambdas)
+#' }
+#'
+#' # Set seed for reproducibility
+#' set.seed(123)
+#'
+#' # Draw n_sim abundances from Poisson distribution using our own function
+#' n_sim <- 10
+#' n_time_points <- 50
+#' slope <- 1
+#' list_abundances <- vector("list", length = n_sim)
+#'
+#' # Loop n_sim times over simulate_timeseries()
+#' for (i in seq_len(n_sim)) {
+#'   abundances <- simulate_timeseries(
+#'     initial_average_abundance = 50,
+#'     n_time_points = n_time_points,
+#'     temporal_autocorr = my_own_linear_function,
+#'     coef = slope)
+#'
+#'   list_abundances[[i]] <- data.frame(
+#'     time = seq_along(abundances),
+#'     abundance = abundances,
+#'     sim = i
+#'     )
+#' }
+#'
+#' # Combine list of dataframes
+#' data_abundances <- do.call(rbind.data.frame, list_abundances)
+#'
+#' # Plot the simulated abundances over time using ggplot2
+#' ggplot(data_abundances, aes(x = time, y = abundance, colour = factor(sim))) +
+#'   geom_line() +
+#'   labs(x = "Time", y = "Species abundance",
+#'   title = paste(n_sim, "simulated abundances using our own linear function",
+#'                 "with slope", slope)) +
+#'   scale_y_continuous(limits = c(0, NA)) +
+#'   scale_x_continuous(breaks = seq(0, n_time_points, 5)) +
+#'   theme_minimal() +
+#'   theme(legend.position = "")
 
 simulate_timeseries <- function(
     initial_average_abundance = 50,
@@ -55,7 +158,8 @@ simulate_timeseries <- function(
   )
   }
   # Check if temporal_autocorr is NA or a function
-  if (!is.na(temporal_autocorr) & !is.function(temporal_autocorr)) {
+  if (suppressWarnings(!is.na(temporal_autocorr)) &
+      !is.function(temporal_autocorr)) {
       cli::cli_abort(c(
       "{.var temporal_autocorr} must be `NA` or a function.",
       "x" = paste("You've supplied a {.cls {class(temporal_autocorr)}}",
@@ -93,8 +197,7 @@ simulate_timeseries <- function(
       lambdas <- temporal_autocorr(
         initial_average_abundance,
         n_time_points,
-        ...,
-        seed = seed)
+        ...)
     }
     timeseries <- rpois(n_time_points, lambdas)
   } else {
