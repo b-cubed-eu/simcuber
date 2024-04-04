d30d81a262491f959b16451ed55f5947ab5763a0
Sanne Govaert <44606923+sangovae@users.noreply.github.com>
2024-04-04 12:33:46 +0200

    add param documentation

--- R/sampling_bias_manual.R
+++ R/sampling_bias_manual.R
@@ -6,9 +6,17 @@
 #'
 #' @param grid An sf object with POLYGON geometry (usually a grid) to which
 #' observations should be designated.
+#' @param bias_weights `NA` or a raster layer (sf object with POLYGON geometry,
+#' or SpatRaster object). Only used if `sampling_bias = "manual"`. The raster
+#' of bias weights to be applied to the sampling of occurrences. Higher weights
+#' mean a higher probability of sampling. Weights can be numeric values between
+#' 0 and 1 or positive integers that will be rescaled to values between 0 and 1.
 #'
+#' @returns ...
+#'
+#' @export
 sampling_bias_manual <- function(
-    grid) {
+    grid, bias_weights) {
   # ...
-}
 
+}
