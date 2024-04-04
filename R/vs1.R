bdbadbd0c54a7ad5c589793e91179589aa648bfe
Rocìo Beatriz Cortès <rociobeatrizcortes@gmail.com>
2024-04-04 10:10:08 +0200

    bioclim var

--- R/vs1.R
+++ R/vs1.R
@@ -5,15 +5,76 @@ library(viridis)
 library(virtualspecies)
 library(tidyverse)
 library(geodata)
-
+library(corrplot)
 # worldclim_global(var, res, path, version="2.1", ...)
 # worldclim_country(country, var, path, version="2.1", ...)
 # worldclim_tile(var, lon, lat, path, version="2.1", ...)
 
 # Download all bioclimatic variables
-d <- worldclim_country(country = "Italy", var = "bio", res=0.5, path=tempdir())
-plot(d)
-plot(d[[5]])
+d <- worldclim_country(country = "Italy", var = "bio", res=0.5, path=tempdir()) %>% stack()
+
+#BIO1 = Annual Mean Temperature
+
+#BIO2 = Mean Diurnal Range (Mean of monthly (max temp - min temp))
+
+#BIO3 = Isothermality (BIO2/BIO7) (×100)
+
+#BIO4 = Temperature Seasonality (standard deviation ×100)
+
+#BIO5 = Max Temperature of Warmest Month
+
+#BIO6 = Min Temperature of Coldest Month
+
+#BIO7 = Temperature Annual Range (BIO5-BIO6)
+
+#BIO8 = Mean Temperature of Wettest Quarter
+
+#BIO9 = Mean Temperature of Driest Quarter
+
+#BIO10 = Mean Temperature of Warmest Quarter
+
+#BIO11 = Mean Temperature of Coldest Quarter
+
+#BIO12 = Annual Precipitation
+
+#BIO13 = Precipitation of Wettest Month
+
+#BIO14 = Precipitation of Driest Month
+
+#BIO15 = Precipitation Seasonality (Coefficient of Variation)
+
+#BIO16 = Precipitation of Wettest Quarter
+
+#BIO17 = Precipitation of Driest Quarter
+
+#BIO18 = Precipitation of Warmest Quarter
+
+#BIO19 = Precipitation of Coldest Quarter
+
+# Subsample 10% of pixels and calculate pairwise correlations
+d
+r1 <- mydata$wc2.1_30s_bio_1
+cor <- cor(sampleRandom(mydata, size= ncell(r1) * 0.10 ), method = "pearson")
+
+
+# Creazione di un vettore di nomi delle variabili
+names_var <- c()
+
+# Numero di variabili da rinominare
+num_var <- 19
+
+# Generazione dei nomi delle variabili
+for (i in 1:num_var) {
+  new_name <- paste("bio", i, sep = "")
+  names_var <- c(names_var, new_name)
+}
+
+# Visualizzazione dei nuovi nomi delle variabili
+print(names_var)
+names(d) <- names_var
+
+# Plot correlation matrix
+df <- corrplot(cor, method = "number")
 
 # Number of random vs
 Number_species <- 3
