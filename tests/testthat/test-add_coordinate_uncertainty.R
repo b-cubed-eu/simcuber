7a9a5f38f6f7e49ffa5b4e642aea97faf17dfb20
PietrH <pieterhuy@gmail.com>
2024-04-04 12:03:03 +0200

    add empty tests

--- tests/testthat/test-add_coordinate_uncertainty.R
+++ tests/testthat/test-add_coordinate_uncertainty.R
@@ -1,3 +1,7 @@
+test_that("add_coordinate_uncertainty() returns sf object", {
+
+})
+
 test_that("add_coordinate_uncertainty() returns error on non sf occurrence input", {
   not_an_sf_object <- data.frame(1:5,5,6)
   expect_error(
@@ -10,4 +14,8 @@ test_that("add_coordinate_uncertainty() returns error on non sf occurrence input
     regexp = "You've supplied an object of class <data.frame>",
     fixed = TRUE
   )
+})
+
+test_that("add_coordinate_uncertainty() returns error when occurrence geompetry is different from POINT", {
+
 })
