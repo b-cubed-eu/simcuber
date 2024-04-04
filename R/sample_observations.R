a39046a0418196d23ea28f14c3b0fa0664f81771
Sanne Govaert <44606923+sangovae@users.noreply.github.com>
2024-04-04 18:48:59 +0200

    add periods

--- R/sample_observations.R
+++ R/sample_observations.R
@@ -81,13 +81,13 @@ sample_observations <- function(
   # To do: checks for bias_area, bias_strength and bias_weights
   if (!("sf" %in% class(occurrences))) {
     cli::cli_abort(c(
-      "{.var occurrences} must be an sf object",
+      "{.var occurrences} must be an sf object.",
       "x" = "You've supplied a {.cls {class(occurrences)}} object."
     ))
   }
   if (!is.numeric(detection_probability)) {
     cli::cli_abort(c(
-      "{.var detection_probability} must be a numeric value between 0 and 1",
+      "{.var detection_probability} must be a numeric value between 0 and 1.",
       "x" = "You've supplied a {.cls {class(detection_probability)}} object."
     ))
   }
@@ -99,23 +99,23 @@ sample_observations <- function(
   }
   if (!is.numeric(coordinate_uncertainty_meters)) {
     cli::cli_abort(c(
-      "{.var coordinate_uncertainty_meters} must be a positive numeric value",
+      "{.var coordinate_uncertainty_meters} must be a positive numeric value.",
       "x" = "You've supplied a {.cls {class(coordinate_uncertainty_meters)}}
       object."
     ))
   }
   if (!is.integer(seed) & !is.na(seed)) {
     cli::cli_abort(c(
-      "{.var seed} must be NA or a positive integer",
-      "x" = "You've supplied a {.cls {class(occurrences)}} object."
+      "{.var seed} must be NA or a positive integer.",
+      "x" = "{.var seed} has the value {seed}."
     ))
   }
 
   # 2. other checks
   # detection_probability is a numeric value between 0 and 1
-  if ((!(0 <= detection_probability) & !(detection_probability <= 1))) {
+  if ((!(0 <= detection_probability) && !(detection_probability <= 1))) {
     cli::cli_abort(c(
-      "{.var detection_probability} must be a numeric value between 0 and 1",
+      "{.var detection_probability} must be a numeric value between 0 and 1.",
       "x" = "You've supplied {.cls {class(detection_probability)}} as
       {.var detection_probability}."
     ))
@@ -123,7 +123,7 @@ sample_observations <- function(
 # seed is a positive value
 if (seed <= 0) {
   cli::cli_abort(c(
-    "{.var seed} must be a positive integer",
+    "{.var seed} must be a positive integer.",
     "x" = "You've supplied {seed} as {.var seed}."
   ))
 }
