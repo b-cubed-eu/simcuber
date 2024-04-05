5979d2172a32d1f59d9753b072a57ec4c4d9f36a
wlangera <ward.langeraert@inbo.be>
2024-04-05 11:25:15 +0200

    rebuild

--- R/sampling_bias_manual.R
+++ R/sampling_bias_manual.R
@@ -63,6 +63,7 @@
 #'  geom_sf_text(data = grid2, aes(label = bias_weight)) +
 #'  geom_sf(data = occurrence_bias_sf, aes(colour = bias_weight)) +
 #'  scale_color_gradient(trans = "reverse")
+
 sampling_bias_manual <- function(occurrences_sf, bias_weights) {
   ### Start checks
   # 1. check input classes
