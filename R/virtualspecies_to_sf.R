c7f861316d1b9cde3a94f47116f9f8ee09f06d07
MatildeUNIBO <matilde.martini7@unibo.it>
2024-04-05 12:35:43 +0200

    add suitability score

--- R/virtualspecies_to_sf.R
+++ R/virtualspecies_to_sf.R
@@ -1,4 +1,4 @@
-#' Create virtual species
+##' Create virtual species
 #'
 #' The function create a given number of virtual species and returns
 #' an sf object with the virtual species occurrences as well as its environmental
@@ -6,14 +6,11 @@
 #'
 #' @param covariates_stack raster stack of the ecological variables from which
 #' the virtual species are created.
-#' @param nocc numeric values indicate the number of desired occurrences for the
-#' virtual species, by default is 100.
-#' virtual species
 #' @param polygon An sf object with POLYGON geometry indicating the spatial
 #' extend to simulate occurrences. If `NA` (the default), the virtual species is
 #' generated in the entire area of the stack.
 #' @param n_time_points A positive integer value indicating the number of time
-#' points to simulate.
+#' points to simulate (years).
 #' @param seed A positive numeric value. The seed for random number generation
 #' to make results reproducible. If `NA` (the default), no seed is used.
 #'
@@ -34,50 +31,71 @@
 #'
 #' stack_bio <- worldclim_country(country = "Belgium", var = "bio", res=0.5, path=tempdir())
 #'
-#' out <- virtualspecies_to_sf(covariates_stack = stack_bio)
+#' output1 <- virtualspecies_to_sf(covariates_stack = stack_bio)
+#' output2 <- virtualspecies_to_sf(covariates_stack = stack_bio, virtualsp_info = T)
+
+
 
 virtualspecies_to_sf <- function(
     covariates_stack,
-    nocc = 100,
     polygon = NA,
-    seed = NA){
+    crs_c = 4326,
+    seed = NA,
+    first_year = 1990,
+    last_year = 2020,
+    n_time_points = 10,
+    virtualsp_info = F){
 
   if(!is.na(polygon)){
     covariates_stack <- covariates_stack %>%
       terra::mask(polygon)
     if(nrow(covariates_stack))
       cli::cli_abort(c(
-      "polygon must overlap the covariates_stack",
-    ))
+        "polygon must overlap the covariates_stack",
+      ))
   }
 
-  random.sp <- generateRandomSp(raster.stack = stack_bio,
+  random.sp <- generateRandomSp(raster.stack = covariates_stack,
                                 convert.to.PA = FALSE,
                                 species.type = "multiplicative",
                                 relations = "gaussian",
                                 realistic.sp = TRUE,
                                 plot = FALSE)
+  random.sp$details
+  suitab_score <- random.sp$suitab.raster
 
   new.pres <- convertToPA(random.sp,
-                         beta = "random",
-                         plot = FALSE)
+                          beta = "random",
+                          plot = FALSE)
 
-  presence.points <- sampleOccurrences(new.pres,
+  # Presence as xy
+  occurrences <- new.pres$pa.raster %>%
+    terra::as.points() %>%
+    st_as_sf() %>%
+    filter(lyr.1 > 0) %>%
+    st_transform(crs = crs_c)
 
-                                       n = 100,
 
-                                       n = nocc,
+  occurrences_ss <- occurrences %>%
+    {ex <- extract(c(stack_bio, suitab_score),.)
+    bind_cols(.,ex)}
 
-                                       type = "presence only",
-                                       sample.prevalence = 0.5,
-                                       detection.probability = 0.8,
-                                       correct.by.suitability = TRUE,
-                                       plot = F)
+  occurrences_ss <- occurrences_ss %>%
+    dplyr::select(-lyr.1, -ID)
 
-  sf_occurrences <- presence.points$sample.points %>%
-    st_as_sf(coords = c("x", "y"), crs = crs)
+  occurrences_ss_time <- runif(nyears, first_year, last_year) %>%
+    round(0) %>%
+    sample(size = nrow(occurrences_ss), replace = T) %>%
+    add_column(occurrences_ss, time_point = .) %>%
+    dplyr::select(everything(), geometry)
 
-  suitab_raster <- random.sp$suitab.raster
+  if(!virtualsp_info){
+    return(virtual_occ = occurrences_ss_time)
+  }else{
+    return(list(virtual_occ = occurrences_ss_time, raster_ss = suitab_score, virtual_sp_info = random.sp$details))
+  }
 
-  return(list(sf_occurrences = sf_occurrences, suitab_raster = suitab_raster))
 }
+
+
+
