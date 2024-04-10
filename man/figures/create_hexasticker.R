5ed0f0bf1012dd6f46ad3d50e668a7ffc946dabf
wlangera <ward.langeraert@inbo.be>
2024-04-10 12:30:52 +0200

    logo size smaller

--- man/figures/create_hexasticker.R
+++ man/figures/create_hexasticker.R
@@ -5,4 +5,4 @@ sticker("man/figures/logo.png", package = "gcube",
         s_x = 1, s_y = 0.7, s_width = 0.5, s_height = 0.5,
         h_fill = "black", h_color = "#48A529",
         url = "https://b-cubed-eu.github.io/gcube/",
-        filename = "man/figures/hexa_sticker.png")
+        filename = "man/figures/hexa_sticker_original.png")
